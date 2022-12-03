3b40a0537f2449c5e1d663146c863573d69fe391
minhle10 <lems@bc.edu>
2022-12-03 15:25:24 -0500

    save model using pickle

--- neuralNet.py
+++ neuralNet.py
@@ -1,9 +1,7 @@
-import sklearn
-import sklearn_json as skljson
 from sklearn.neural_network import MLPClassifier as MLPC
-from sklearn.datasets import load_digits # X This is just to tes
+from sklearn.datasets import load_digits # X This is just to test
+import pickle
 
-file_name = "neuralModelWeights.json"
 
 D = load_digits()
 model = MLPC(hidden_layer_sizes=(100,50,25))
@@ -11,8 +9,9 @@ model.fit(D.data, D.target)
 accuracy = model.score(D.data, D.target)
 print("accuracy: ", accuracy)
 
-skljson.to_json(model, file_name)
-savedModel = skljson.from_json(file_name)
+# Saves nn weights into nnWeights.sav
+fileName = "nnWeights.sav"
+pickle.dump(model, open(fileName, "wb"))
 
-smAccuracy = savedModel.score(D.data, D.target)
-print("saved model accuracy: ", accuracy)
+# loads nn weights from nnWeights.sav
+loadedModel = pickle.load(open(fileName, 'rb'))
